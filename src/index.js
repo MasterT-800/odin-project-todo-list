0896f07462dfd03a32a69425ae9f375beec16880
tlinux <115206437+MasterT-800@users.noreply.github.com>
2024-10-05 13:40:55 -0400

    Create add project function
    Function creates a new project adds it to the end
    of the existing project list and saves it to local
    storage.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { storeObject, getObject } from "./storage.js";
         //Retrieve locally stored list and save to it variable
         const retrievedList = getObject('project list');
         console.log(retrievedList);
+        //Create new Project objects out of retrieved json object
+        //Returns method funtionality to stored objects
         //Display list of projects
     }
     //If not load new default empty project
@@ -17,13 +19,35 @@ import { storeObject, getObject } from "./storage.js";
         //Create empty project list array and add default project to it
         const projectList = [];
         const defaultProject = new Project('Default Project');
+        projectList.push(defaultProject);
         //Save project list to local storage
         storeObject(projectList, 'project list');
         //Display project
         
     }
   })();
+  
+//Restores method functionality to objects rertrieved from local storage
+function restoreMethods(project){
 
-  function displayList(list){
+}
+
+function displayList(list){
     //Iterates through stored list and adds it to dom
+    //Iterate through each todo list and add each item to dom
+    //Add edit and delete buttons to todos
+}
+
+//Create new todo to project then save to local storage
+function addTodo(){
+    //Add to todo list in project
+    //Save to local storage
+}
+//Create new project and save to local storage
+function addProject(title){
+    const newProject = new Project(title);
+    //Save to storage
+    const retrievedList = getObject('project list');
+    retrievedList.push(newProject);
+    storeObject(retrievedList, 'project list');
   }
